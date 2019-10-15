dfb0f24044e72f6b89d52b45045dcb90e3bda891
monsef alahem <m.alahem09@gmail.com>
2019-10-15 15:50:12 +0100

    Add files via upload

---
+++ child_page.js
@@ -0,0 +1,62 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_6_ENPnJpY5j3UQfuEf1pWinaI-NQXuk",
+    authDomain: "monsef-alahem.firebaseapp.com",
+    databaseURL: "https://monsef-alahem.firebaseio.com",
+    projectId: "monsef-alahem",
+    storageBucket: "monsef-alahem.appspot.com",
+    messagingSenderId: "438113761415",
+    appId: "1:438113761415:web:236f057fc7550146e32022"
+  };
+
+document.addEventListener('DOMContentLoaded', function() { 
+    printview()
+})
+function get_reference (val) {
+    if (val == "linux") {
+        return "vue_linux"
+    } else if (val == "mes programmes") {
+        return "vue_prog"
+    } else if (val == "autoit") {
+        return "vue_autoit"
+    } else if (val == "C") {
+        return "vue_c"
+    } else if (val == "C++") {
+        return "vue_c++"
+    } else if (val == "batch") {
+        return "vue_batch"
+    } else if (val == "problèmes & solutions") {
+        return "vue_prosol"
+    } else if (val == "python") {
+        return "vue_python"
+    } else if (val == "Accueil") {
+        return "vue_choaib"
+    }
+}
+
+function printview() {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var refs = firebase.database().ref(get_reference(document.title));
+
+        //read
+        sleep(100);
+        refs.once('value', snap => {
+        var x = snap.val();
+        x++;
+        //write
+        document.getElementById('views_counter').innerHTML = x;
+        var views = parseInt(document.getElementById('views_counter').innerHTML);
+        refs.set(x);
+        document.getElementById('views_counter').innerHTML = ("consulté " + x + " fois");
+        });
+}
+
+function sleep(millis)
+{
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < millis);
+}
--- index.js
+++ index.js
@@ -1,7 +1,102 @@
+//<!-- TODO: Add SDKs for Firebase products that you want to use 
+//https://firebase.google.com/docs/web/setup#available-libraries -->
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_6_ENPnJpY5j3UQfuEf1pWinaI-NQXuk",
+    authDomain: "monsef-alahem.firebaseapp.com",
+    databaseURL: "https://monsef-alahem.firebaseio.com",
+    projectId: "monsef-alahem",
+    storageBucket: "monsef-alahem.appspot.com",
+    messagingSenderId: "438113761415",
+    appId: "1:438113761415:web:236f057fc7550146e32022"
+  };
+                          /*  // Initialize Firebase
+                            firebase.initializeApp(firebaseConfig);
+
+                            function number(value) {
+                              this.v = value;
+                            }
+
+                            var data = new number(0);
+
+                            var refs = firebase.database().ref('vue');
+
+
+                            //read
+                              //window.onload = function() {
+                                sleep(1000);
+                                  refs.on('value', snap => {
+                                  var x = snap.val();
+                                  x++;
+                                  document.getElementById('views_counter').innerHTML = x;
+                                  var views = parseInt(document.getElementById('views_counter').innerHTML);
+                                  alert(x + '' + views);
+                                  refs.set(x);
+                                  //var temp = {v: 0};
+                                  //refs.set(temp.v);
+                                  });//snap.val()*/
+   // }
+        //innerHTML
+        //innerText
+/*
+        ref.once('value').then(function(data) {
+        var temp = {v: 0};
+        temp.v = data.val();
+        data.v = data.val();
+        temp.v++;
+        alert(temp.v);
+      });
+*/
+    //alert("data"+data.v);
+      //read
+/*    window.onload = function() {
+        var temp = {v: 11};
+        refs.set(document.getElementById('views_counter').innerHTML);
+        //refs.set(111);
+        alert(document.getElementById('views_counter').innerHTML);
+    }*/
+
+
+
+  //write
+  function increase(temp) { //increase
+    //database.ref('vue').set(temp.v);
+  }
+
+  //increase(data);
+
+/*  starCountRef.on('value', function(snapshot) {
+    updateStarCount(postElement, snapshot.val());
+  });*/
+
+/*  function get_vue(firebase) {
+    var userId = firebase.auth().currentUser.uid;
+    return firebase.database().ref('vue').once('value').then(function(snapshot) {
+      var username = (snapshot.val);
+    });
+  }*/
+
+    //var data = dataSnapshot.node_.children_.root_.value.value_;
+    //alert("consulté " + data + " fois.");
+    // handle read data.
+  
+  //alert(get_vue());
+
+
+
+
+
+
+
+
+  //===========================================================================
+  //===========================================================================
+
 audio = new Audio('CSI_Open.wav');
 document.addEventListener('DOMContentLoaded', function() { 
     audio.play()
-    printWelcome()
+    printview()
 })
 
 
@@ -12,8 +107,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /*window.addEventListener('resize', resizeCanvas, false)*/
 
+function printview() {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function number(value) {
+    this.v = value;
+  }
+
+  var data = new number(0);
+
+  var refs = firebase.database().ref('vue_index');
+
+
+  //read
+    //window.onload = function() {
+        sleep(100);
+        refs.once('value', snap => {
+        var x = snap.val();
+        x++;
+        document.getElementById('views_counter').innerHTML = x;
+        var views = parseInt(document.getElementById('views_counter').innerHTML);
+        //alert(x + '' + views);
+        refs.set(x);
+        document.getElementById('views_counter').innerHTML = ("consulté " + x + " fois");
+        //var temp = {v: 0};
+        //refs.set(temp.v);
+        });//snap.val()
+   // }
+
+  // var welcomemsg = new Typed('.views_counter', {
+  //       strings : ['consulté ' + data.v + ' fois'],
+  //       stringsElement: null, 
+  //       typeSpeed: 20, 
+  //       backSpeed: -1,
+  //       showCursor: true,
+  //       cursorChar: '\u2588',
+  //       autoInsertCss: true,
+  //       onComplete: (self) => {
+  //          $('.typed-cursor').hide()
+  //           /*/sleep(2000)*/
+           audio.play();
+            printWelcome();
+    //     }
+    // });
+}
+
 function printWelcome() {
-  var welcomemsg = new Typed('.welcomemsg', {
+    var welcomemsg = new Typed('.welcomemsg', {
         strings : ['Bienvenu'],
         stringsElement: null, 
         typeSpeed: 20, 
@@ -46,11 +187,11 @@ function printpar() {
         }
     });  
 }
-/*
+
 function sleep(millis)
 {
     var date = new Date();
     var curDate = null;
     do { curDate = new Date(); }
     while(curDate-date < millis);
-}*/
+}
