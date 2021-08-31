b93b2894fcf40e1ff692d5f861e0246626bde147
monsef alahem <m.alahem09@gmail.com>
2021-08-31 17:09:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,35 +11,7 @@
     messagingSenderId: "438113761415",
     appId: "1:438113761415:web:236f057fc7550146e32022"
   };
-                          /*  // Initialize Firebase
-                            firebase.initializeApp(firebaseConfig);
-
-                            function number(value) {
-                              this.v = value;
-                            }
-
-                            var data = new number(0);
-
-                            var refs = firebase.database().ref('vue');
-
-
-                            //read
-                              //window.onload = function() {
-                                sleep(1000);
-                                  refs.on('value', snap => {
-                                  var x = snap.val();
-                                  x++;
-                                  document.getElementById('views_counter').innerHTML = x;
-                                  var views = parseInt(document.getElementById('views_counter').innerHTML);
-                                  alert(x + '' + views);
-                                  refs.set(x);
-                                  //var temp = {v: 0};
-                                  //refs.set(temp.v);
-                                  });//snap.val()*/
-   // }
-        //innerHTML
-        //innerText
-/*
+                         
         ref.once('value').then(function(data) {
         var temp = {v: 0};
         temp.v = data.val();
@@ -47,47 +19,6 @@
         temp.v++;
         alert(temp.v);
       });
-*/
-    //alert("data"+data.v);
-      //read
-/*    window.onload = function() {
-        var temp = {v: 11};
-        refs.set(document.getElementById('views_counter').innerHTML);
-        //refs.set(111);
-        alert(document.getElementById('views_counter').innerHTML);
-    }*/
-
-
-
-  //write
-  function increase(temp) { //increase
-    //database.ref('vue').set(temp.v);
-  }
-
-  //increase(data);
-
-/*  starCountRef.on('value', function(snapshot) {
-    updateStarCount(postElement, snapshot.val());
-  });*/
-
-/*  function get_vue(firebase) {
-    var userId = firebase.auth().currentUser.uid;
-    return firebase.database().ref('vue').once('value').then(function(snapshot) {
-      var username = (snapshot.val);
-    });
-  }*/
-
-    //var data = dataSnapshot.node_.children_.root_.value.value_;
-    //alert("consulté " + data + " fois.");
-    // handle read data.
-  
-  //alert(get_vue());
-
-
-
-
-
-
 
 
   //===========================================================================
@@ -99,14 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
     printview()
 })
 
-
-/*audio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-})*/
-
-/*window.addEventListener('resize', resizeCanvas, false)*/
-
 function printview() {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -116,41 +39,19 @@ function printview() {
   }
 
   var data = new number(0);
-
   var refs = firebase.database().ref('vue_index');
 
-
   //read
-    //window.onload = function() {
-        sleep(100);
-        refs.once('value', snap => {
-        var x = snap.val();
-        x++;
-        document.getElementById('views_counter').innerHTML = x;
-        var views = parseInt(document.getElementById('views_counter').innerHTML);
-        //alert(x + '' + views);
-        refs.set(x);
-        document.getElementById('views_counter').innerHTML = (x + " views");
-        //var temp = {v: 0};
-        //refs.set(temp.v);
-        });//snap.val()
-   // }
-
-  // var welcomemsg = new Typed('.views_counter', {
-  //       strings : [data.v + ' views'],
-  //       stringsElement: null, 
-  //       typeSpeed: 20, 
-  //       backSpeed: -1,
-  //       showCursor: true,
-  //       cursorChar: '\u2588',
-  //       autoInsertCss: true,
-  //       onComplete: (self) => {
-  //          $('.typed-cursor').hide()
-  //           /*/sleep(2000)*/
-           audio.play();
-            printWelcome();
-    //     }
-    // });
+  sleep(100);
+  refs.once('value', snap => {
+  var x = snap.val();
+  x++;
+  document.getElementById('views_counter').innerHTML = x;
+  var views = parseInt(document.getElementById('views_counter').innerHTML);
+  refs.set(x);
+  document.getElementById('views_counter').innerHTML = (x + " views");
+ 
+  printWelcome();
 }
 
 function printWelcome() {
@@ -164,7 +65,6 @@ function printWelcome() {
         autoInsertCss: true,
         onComplete: (self) => {
            $('.typed-cursor').hide()
-            /*/sleep(2000)*/
            audio.play()
             printpar()
         }
@@ -188,8 +88,7 @@ function printpar() {
     });  
 }
 
-function sleep(millis)
-{
+function sleep(millis) {
     var date = new Date();
     var curDate = null;
     do { curDate = new Date(); }
